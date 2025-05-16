d4855b5900593891537d73806f1ccc19b70c0cda
Jenny Taranto <jennytaranto.pro@gmail.com>
2025-05-16 16:10:50 +0200

    pages Projets terminées

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,10 +1,11 @@
 
-function Card( { cover, title }) {
+function Card({ cover, title }) {
     return (
         <div className="card">
-            <img src={cover} alt={title} className="cover"/>
+            <img src={cover} alt={title} className="cover" />
             <p>{title}</p>
-            <img src="/Logo_OpenClassrooms.png" alt="Logo Openclassrooms" className="logooc"/>
+            <img src="/Logo_OpenClassrooms.png" alt="Logo Openclassrooms" className="logooc" />
+            <div className="card__text">VOIR FICHE</div>
         </div>
     )
 }
--- src/pages/Project/Project.jsx
+++ src/pages/Project/Project.jsx
@@ -1,19 +1,42 @@
+import { useParams, Navigate } from "react-router-dom";
+import Carrousel from "../../components/Carrousel/Carrousel";
+import Projects from "../../data/projects.json";
 
-function Home() {
+function Project() {
+  const { id } = useParams();
+  const Project = Projects.find((item) => item.id === id);
+
+  if (!Project) {
+    return <Navigate to="/Error" />
+  }
+
   return (
-    <main className="main-home"> 
-      <div>
+    <main className="main-project">
+      <section className="section-one">
+        <div className="title-link">
+          <h3 className="project-title">{Project.subtitle}</h3>
+          <p>Code sur <a href={Project.link}>Github</a></p>
+        </div>
+        <Carrousel pictures={Project.pictures} />
+        <p className="description-title">Description</p>
+        <p className="text">{Project.description}</p>
+        <p className="technology-title">Technologies utilisées</p>
+        <ul className="technology-list">
+          {Project.technology.map((tech, index) => (
+            <li key={index}>{tech}</li>
+          ))}
+        </ul>
+      </section>
+      <section className="section-two">
+        <h3 className="second-title">CE QUE J'AI APPRIS</h3>
+        <p>{Project.conclusion}</p>
+      </section>
+      <div className="formation">
+        <p>Projet réalisé dans le cadre d'une formation Openclassrooms</p>
+        <img src="/Logo_OpenClassrooms.png" alt="Logo Openclassrooms" />
       </div>
-      <section>
-        <h2></h2>
-        
-      </section>
-      <section>
-        <h2></h2>
-        <p></p>
-      </section>
     </main>
   );
 };
 
-export default Home
+export default Project
