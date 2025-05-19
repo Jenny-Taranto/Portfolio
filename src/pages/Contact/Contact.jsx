b6af0b97fc10a6178628dd5f3467f8af5e0b80d3
Jenny Taranto <jennytaranto.pro@gmail.com>
2025-05-19 18:40:10 +0200

    form

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -13,7 +13,7 @@ function Contact() {
       body: data,
     })
       .then(() => setSubmitted(true))
-      .catch((error) => alert("Une erreur est survenue, veuillez réessayer."));
+      .catch(() => alert("Une erreur est survenue, veuillez réessayer."));
 
     form.reset();
   };
@@ -22,13 +22,23 @@ function Contact() {
     <main className="main-contact">
       <h2>Contact</h2>
       <div className="container">
-        <img src="/paper-plane.png" alt="Avion en papier" className="paper-plane" />
+        <img
+          src="/paper-plane.png"
+          alt="Avion en papier"
+          className="paper-plane"
+        />
         <p>Une question ? Un projet ? Lancez-vous.</p>
 
         <div className="socials">
-          <a href="mailto:contact@jennytaranto.com"><img src="/enveloppe.png" alt="Enveloppe" /></a>
-          <a href="https://github.com/Jenny-Taranto" target="_blank"><img src="/github.png" alt="Logo Github" /></a>
-          <a href="https://linkedin.com/in/jenny-taranto-a81631327" target="_blank"><img src="/linkedin.png" alt="Logo LinkedIn" /></a>
+          <a href="mailto:contact@jennytaranto.com">
+            <img src="/enveloppe.png" alt="Enveloppe" />
+          </a>
+          <a href="https://github.com/Jenny-Taranto" target="_blank">
+            <img src="/github.png" alt="Logo Github" />
+          </a>
+          <a href="https://linkedin.com/in/jenny-taranto-a81631327" target="_blank">
+            <img src="/linkedin.png" alt="Logo LinkedIn" />
+          </a>
         </div>
 
         {submitted ? (
@@ -42,9 +52,12 @@ function Contact() {
             action="/"
             onSubmit={handleSubmit}
           >
+            {/* Anti-spam */}
             <input type="hidden" name="form-name" value="contact" />
             <p hidden>
-              <label>Ne pas remplir ce champ : <input name="bot-field" /></label>
+              <label>
+                Ne pas remplir ce champ : <input name="bot-field" />
+              </label>
             </p>
 
             <label htmlFor="name">Votre nom</label>
@@ -64,6 +77,6 @@ function Contact() {
       </div>
     </main>
   );
-};
+}
 
 export default Contact;
